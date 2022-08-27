e387103beb7db2002bcf08d8cf4bec6112d597b4
Augustine Tepet <azaah1975@gmail.com>
2022-08-26 21:13:22 -0400

    testing branching

--- movie1.tf
+++ movie1.tf
@@ -1,3 +1,6 @@
 # the name of the first movie is street kufu1
 # i need more  movies uploaded in this project
-git 
+i love good data "nsxt_switching_profile" "qos_profile" {
+  display_name = "qos-profile"
+looking good
+working alot.
